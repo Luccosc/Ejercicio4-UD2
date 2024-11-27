96fdad80444d3f74e377786c3fa47a10fcc629fd
Lucca <luccasolerramos@gmail.com>
2024-11-27 17:41:01 +0100

    Subo proyecto1

---
+++ Proyecto1/src/area/Area.java
@@ -0,0 +1,22 @@
+package area;
+
+import java.util.Scanner;
+
+public class Area {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Introduce el radio del circulo: ");
+		double radio = entrada.nextDouble();
+		
+		double area = Math.PI * Math.pow(radio, 2);
+		System.out.print("El area del circulo es: " + area);
+		
+		
+		
+		entrada.close();
+		
+
+	}
+
+}
---
+++ Proyecto1/src/snippet/Snippet.java
@@ -0,0 +1,6 @@
+package snippet;
+
+public class Snippet {
+	git remote add origin https://github.com/Luccosc/Ejercicio4-UD2.git
+}
+
